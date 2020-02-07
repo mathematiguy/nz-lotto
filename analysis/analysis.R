5b7f64a93c7f29b6002f979709e6190cd4135fc9
Caleb Moses <caleb@dragonfly.co.nz>
2020-02-08 01:31:08 +1300

    Tidy up the raw json data

---
+++ analysis/analysis.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(jsonlite)
+library(here)
+library(furrr)
+
+plan(multiprocess)
+
+results <- read_json(here("lotto/output.json"))
+
+results_data <- results %>%
+    future_map(function(res) {
+    as_tibble(res) %>%
+    select(-results_table) %>%
+    distinct() }) %>%
+    bind_rows()%>%
+    mutate(date = as.Date(date, format = "%d-%B-%Y"),
+           number_of_winners = str_remove_all(number_of_winners, ","),
+           average_prize_per_winner = str_remove(average_prize_per_winner, "\\$"),
+           total_prize_pool = str_remove_all(total_prize_pool, "\\$|,")) %>%
+    mutate_at(vars(starts_with("ball"), "bonus", "powerball", starts_with("strike"),
+                   "draw_number", "number_of_winners"), as.integer) %>%
+    mutate_at(vars("total_prize_pool", "average_prize_per_winner"), as.numeric)
+
+division_data <- results %>%
+    future_map(function (res) {
+    out <- map(res$results_table, as_tibble) %>%
+           bind_rows()
+    out$draw_number <- rep(res$draw, nrow(out))
+    out}) %>%
+    bind_rows() %>%
+    mutate(matches = matches %>%
+    str_trim() %>%
+    str_replace_all("\\s{2,}", " "),
+    winners = str_remove_all(winners, ","),
+    prize = str_remove_all(prize, "\\$|,"),
+    payout = str_remove_all(payout, "\\$|,"),
+    free_ticket = is.na(as.numeric(prize))) %>%
+    mutate_at(vars("division", "winners", "draw_number"), as.integer) %>%
+    mutate_at(vars("prize", "payout"), as.numeric) %>%
+    mutate(prize = if_else(is.na(prize), 0, prize),
+           game = case_when(
+                str_detect(matches, "Powerball") ~ "powerball",
+                str_detect(matches, "Exact") ~ "strike",
+                TRUE ~ "lotto"))
