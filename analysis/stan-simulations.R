9a6a1bbc29b2fc47780994ae03b3044b82145ae3
Caleb Moses <caleb@dragonfly.co.nz>
2020-02-13 01:10:18 +1300

    Update stan-simulations.R

--- analysis/stan-simulations.R
+++ analysis/stan-simulations.R
@@ -8,6 +8,10 @@ rstan_options(auto_write = TRUE)
 prior_simulations <- stan(
     here('analysis/simulations.stan'),
     model_name = 'prior_simulations',
-    data = list(K = 40, alpha = 1), algorithm = 'Fixed_param')
+    data = list(K = 40, N=6, alpha = 1), algorithm = 'Fixed_param')
 
-predictions <- extract(prior_simulations)$theta
+theta <- extract(prior_simulations)$theta
+ranking <- extract(prior_simulations)$ranking
+results <- extract(prior_simulations)$results
+
+results
