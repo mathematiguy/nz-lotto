26678008481a22f605fdfc028a78ba5afe5501c0
Caleb Moses <caleb@dragonfly.co.nz>
2020-02-12 23:38:34 +1300

    Update analysis.R

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -9,11 +9,10 @@ results_data <- read_csv(here("data/results_data.csv"))
 division_data <- read_csv(here("data/division_data.csv"))
 
 results_data %>%
-    select_at(vars(starts_with("ball"))) %>%
+    select_at(vars(starts_with("ball"), 'bonus')) %>%
     gather(key = "ball", value = "result") %>%
     group_by(result) %>%
     count() %>%
     ggplot(aes(x = result, y = n)) +
-    geom_bar(stat = 'identity')
-
-
+    geom_bar(stat = 'identity') +
+    theme_minimal()
