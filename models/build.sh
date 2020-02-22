626ff3a612082e0bce28af60595bd29a8bfdd7d4
Caleb Moses <caleb@dragonfly.co.nz>
2020-02-22 15:43:31 +1300

    Step up to project directory, then run stuff

--- models/build.sh
+++ models/build.sh
@@ -4,10 +4,11 @@ set -ex
 
 export RUN= 
 
+cd ..
+
 cp /input/nz-lotto-scraper/output.json lotto/output.json
 cp /input/nz-lotto-scraper/*.csv data
 
-cd ..
 make models/simulation_model.rds
 
 cp data/*.csv /output
